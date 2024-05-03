60dade012b9847d9de275d0ae1d68711c835dbd4
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2024-05-03 16:13:24 +0200

    Initial commit
    Signed-off-by: Radoslaw Jurczak <radoslawjurczak95@gmail.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="signatures",
+    version="0.0.0",
+    description="Python package for cancer mutational signature extraction (work in progress)",
+    author="Radosław Jurczak",
+    author_email="r.jurczak@uw.edu.pl",
+    url="https://github.com/czeslaw-milosz/signatures",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    setup_requires=["wheel"]
+)
