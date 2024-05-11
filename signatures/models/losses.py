5fd2d3f713adfe7563b9ccc5de1754afef94bd94
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2024-05-11 02:14:32 +0200

    Implement loss functions for VAE/ProdLDA
    Signed-off-by: Radoslaw Jurczak <radoslawjurczak95@gmail.com>

--- signatures/models/losses.py
+++ signatures/models/losses.py
@@ -3,19 +3,37 @@ from torch import distributions
 
 
 def reconstruction_loss(targets: torch.Tensor, outputs:torch.Tensor) -> torch.Tensor:
-    return -torch.sum(targets*outputs)  # Negative log-likelihood
+    return (-torch.sum(targets*outputs + 1e-10)).mean()  # Negative log-likelihood
 
 
 def kld_loss(posterior: distributions.Distribution, prior: distributions.Distribution) -> torch.Tensor:
-    return torch.sum(distributions.kl_divergence(posterior, prior))
+    return distributions.kl_divergence(posterior, prior).mean()
 
 
-def variational_loss(inputs: torch.Tensor, model: torch.Module, device: str = "cuda:0") -> tuple[torch.Tensor, torch.Tensor]:
-    inputs = inputs.to(device)
-    outputs, posterior = model(inputs)
+def variational_loss(
+        inputs: torch.Tensor, 
+        model_outputs: torch.Module, 
+        posterior: distributions.Distribution,
+        nll_weight: float = 1.,
+        kl_weight: float = 1.,
+        device: str = "cuda:0") -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """Variational loss for ProdLDA VAE.
+    
+    Reconstruction component: negative log-likelihood; probabilistic component: Kullback-Leibler divergence.
+
+    Args:
+        inputs (torch.Tensor): _description_
+        model (torch.Module): _description_
+        nll_weight (float): weight of the reconstruction component of the loss. Defaults to 1.
+        kl_weight (float): weight of the K-L component of the loss. Defaults to 1.
+        device (_type_, optional): device. Defaults to "cuda:0".
+
+    Returns:
+        tuple[torch.Tensor, torch.Tensor]: reconstruction loss, K-L divergence.
+    """
     prior = standard_prior_like(posterior)
-    nll = reconstruction_loss(inputs, outputs)
-    kld = torch.sum(kld_loss(posterior, prior).to(device))
+    nll = reconstruction_loss(inputs, model_outputs) * nll_weight
+    kld = kld_loss(posterior, prior).to(device) * kl_weight
     return nll, kld
 
 
